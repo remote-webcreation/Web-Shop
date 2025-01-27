07b4c99a546edf5e63132769882264b0e76811d3
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-01-27 15:53:55 +0100

    added sidebar on menu toggle for smal screens

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//  toggle menu
+let menu = document.querySelector('#menu-icon');
+
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('navlist-open');
+}
