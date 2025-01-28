17df74facb6a333435a60c230b586be60aaf7a50
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-01-28 14:04:45 +0100

    added partners section

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+// Header sticky
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+
+
 //  toggle menu
 let menu = document.querySelector('#menu-icon');
 
 let navlist = document.querySelector('.navlist');
 
+// on click
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('navlist-open');
 }
+
+// on scroll
 
 window.onscroll = () => {
     menu.classList.remove('bx-x');
